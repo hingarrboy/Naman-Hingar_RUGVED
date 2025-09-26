5ef8e0a56fe5187ad9856e4afc45d7166ad8a798
Naman Hingar <namanhingar22@gmail.com>
2025-09-26 19:35:29 +0530

    Update que12.py

--- TASK1/que12.py
+++ TASK1/que12.py
@@ -11,7 +11,8 @@ for i in range(n-1,-1,-1):
     for j in range(i+1):
         print("* ", end="")
     print()
-    for i in range(1, n + 1):
+
+for i in range(1, n + 1):
     for j in range(1, i + 1):
         print("*", end=" ")
     for j in range(1, 2 * (n - i) + 1):
@@ -23,9 +24,9 @@ for i in range(n-1,-1,-1):
 
 for i in range(n,0,-1):
     for j in range(1,i+1):
-        print("*",end="")
+        print("* ",end="")
     for j in range(1,2*(n-i)+1):
         print(" ",end=" ")
     for j in range(1,i+1):
-        print("*",end=" ")
-    print()
+        print("*", end=" ")
+    print()
